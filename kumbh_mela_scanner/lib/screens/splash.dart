838d1807847652b4d9b11b652bb0c49ee5d5d304
kumarianushka79701 <kumarianushka79701@gmail.com>
2024-12-17 12:17:57 +0530

    new directories

---
+++ kumbh_mela_scanner/lib/screens/splash.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SpalshView extends StatefulWidget {
+  const SpalshView({super.key});
+
+  @override
+  State<SpalshView> createState() => _SpalshViewState();
+}
+
+class _SpalshViewState extends State<SpalshView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
